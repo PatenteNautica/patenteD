d63d83b738b32bc6f9d60dc590eae553cf831771
PatenteNautica <email.luca.martinelli@gmail.com>
2025-04-24 06:21:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ test_structure = {
     "COLREG": 2,
     "METEOROLOGIA": 1,
     "NAVIGAZIONE": 1,
-    "NORMATIVA DIPORTISTICA E AMBIENTALE": 3
+    "NORMATIVA DIPORTISTICA E AMBIENTALE": 2
 }
 
 st.title("Simulatore Esame - Patente D")
